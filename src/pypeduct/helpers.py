1559174cfa0718d3b2147c1f5ff0ba59b1182585
Anselm Kiefner <github@anselm.kiefner.de>
2025-03-07 10:03:26 +0100

    refactor helper functions for clarity and functionality

--- src/pypeduct/helpers.py
+++ src/pypeduct/helpers.py
@@ -29,7 +29,7 @@ def _is_sequence_annotation(annot: AST | None) -> bool:
             return False
 
 
-def _ensure_loc[T: NODE](new: T, ref: AST) -> T:
+def ensure_loc[T: NODE](new: T, ref: AST) -> T:
     new.lineno = getattr(ref, "lineno", 1)
     new.end_lineno = getattr(ref, "end_lineno", new.lineno)
     new.col_offset = getattr(ref, "col_offset", 0)
@@ -37,5 +37,34 @@ def _ensure_loc[T: NODE](new: T, ref: AST) -> T:
     return new
 
 
-def _requires_unpack(left: ast.expr, annotation: AST | None) -> bool:
-    return isinstance(left, ast.Tuple) and not _is_sequence_annotation(annotation)
+def should_unpack(annotation: ast.AST) -> bool:
+    """
+    Examine an AST node representing an annotation and decide if
+    the piped value should be unpacked (via a Starred node) based on it.
+
+    Returns True if the annotation indicates a type that is a Sequence
+    (e.g. list, tuple, or Sequence) and is not one of the types (str, bytes)
+    that should not be unpacked.
+    """
+    # If annotation is a simple name (e.g. list, tuple, Sequence)
+    if isinstance(annotation, ast.Name):
+        if annotation.id in {"list", "tuple", "Sequence", "List", "Tuple"}:
+            return True
+        if annotation.id in {"str", "bytes"}:
+            return False
+
+    # If annotation is subscripted (like list[int] or tuple[int, ...]),
+    # inspect the underlying type.
+    if isinstance(annotation, ast.Subscript):
+        return should_unpack(annotation.value)
+
+    # If annotation is an attribute (like typing.Sequence or collections.abc.Sequence),
+    # check the attribute name.
+    if isinstance(annotation, ast.Attribute) and annotation.attr in {
+        "Sequence",
+        "List",
+        "Tuple",
+    }:
+        return True
+
+    return True
